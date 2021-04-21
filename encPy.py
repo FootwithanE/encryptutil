936464eb9d144a16cba89e0c8ab1b3af082becb1
FootwithanE <sfootew@gmail.com>
2021-04-20 21:58:34 -0700

    Cleaned and commented. Need to time iterations.

--- encPy.py
+++ encPy.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
+"""
+@author: Stephen Foote
+
+April 22, 2021
+
+This python file can be used in conjunction with the associated configuration
+file to encrypt and decrypt files using an end-user provided password.
+
+This program supports AES128, AES256, and Triple DES (3DES) along with HMAC-HASH
+SHA256 or SHA512.
+"""
+
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import HMAC
 from Crypto.Random import get_random_bytes
@@ -8,157 +21,199 @@ import argparse
 import pickle
 import sys
 
-'''
-Function sets, retrieves, and parses command line arguments
-'''
 def get_arguments():
+    """
+    This function is used to collect the end-user provided, commandline
+    arguments. Information regarding the required commandline arguments
+    can also be accessed through the -h or --help commands.
     
-    # Initialize Parser
+    :return: namespace object with provided arguments
+    """    
+    # Initialize Parser object
     parser = argparse.ArgumentParser(description="Encrypt a file.")
     
     # Adding commandline arguments
     parser.add_argument('-m', '--mode', help="Encrypt or Decrypt (e or d)")
-    parser.add_argument('-f', '--filepath', help="Absolute filepath")
+    parser.add_argument('-f', '--filepath', help="Absolute filepath / filename")
     parser.add_argument('-p', '--password', help="File password")
-    parser.add_argument('-o', '--output', help="Decrypted file name")
+    parser.add_argument('-o', '--output', help="Decrypted filename")
     
-    # Parse and add to object
+    # Parse commandline and add to options object
     args = parser.parse_args()
     
-    # If less than two arguments provided
+    # If less than three arguments provided
     if len(sys.argv) < 3:
         print('Not all arguments provided.')
         sys.exit
     
     return args
 
-'''
-Function reads arguments from config file and passes them as serialized dictionary
-'''
 def config_handler():
-    args = {}
-    # Retrieve config information
+    '''
+    Function reads arguments provided in the configuration file and builds
+    a header with information needed to complete encryption and decryption.
+    
+    :return: dictionary with required header information
+    '''
+    # Retrieve config file information
     config = configparser.ConfigParser()
     config.read('config.cfg')
+    
     hashmode = config['settings']['hashmode']
     crypto_alg = config['settings']['encryptmode']
     file_type = config['settings']['outputfile']
-    args["iv_size"] = 8
+    iterations = int(config['settings']['iterations'])
+    kdf_type = config['settings']['kdftype']
+    iv_size = 8
     key_size = 24
+    
+    # Make changes to IV and Key sized based on algorithm selected
     if 'aes' in crypto_alg.lower():
         crypto_alg = crypto_alg[:3]
-        args["iv_size"] = 16
+        iv_size = 16
         if '128' in crypto_alg:
             key_size = 16
         else:
             key_size = 32
     else:
         crypto_alg = crypto_alg
+    
+    # Build header with algorithm information
+    header = {}
+    header["iv_size"] = iv_size
+    header["salt"] = get_random_bytes(48)
+    header["count"] = iterations
+    header["extension"] = file_type    
+    header["key_size"] = key_size
+    header["kdf_type"] = kdf_type
+    
     # dynamically import the appropriate libraries
-    args["crypto_alg"] = importlib.import_module('Crypto.Cipher.' + crypto_alg)
-    args["hashmode"] = importlib.import_module('Crypto.Hash.' + hashmode)
-    args["salt"] = get_random_bytes(48)
-    args["key_size"] = key_size
-    args["count"] = 100000
-    args["extension"] = file_type
+    header["crypto_alg"] = importlib.import_module('Crypto.Cipher.' + crypto_alg)
+    header["hashmode"] = importlib.import_module('Crypto.Hash.' + hashmode)
                  
-    return args
+    return header
 
-'''
-Encrypt a file and return bytes to write
-'''
-def encrypt_file(data, password, args):
-    # Generate the master key
-    m_key = PBKDF2(password, args["salt"][0:16], args["key_size"], count=args["count"], hmac_hash_module=args["hashmode"])
-    # Generate encryption key
-    e_key = PBKDF2(m_key, args["salt"][16:32], args["key_size"], count=1, hmac_hash_module=args["hashmode"])
-    h_key = PBKDF2(m_key, args["salt"][32:], args["key_size"], count=1, hmac_hash_module=args["hashmode"])
+def encrypt_file(data, password, header):
+    '''
+    Encrypts a file and return bytes to write to new file. Header dictionary is built out with
+    remaining necessary information and encrypted data is added to the dictionary. The dictionary is
+    then serialized in order to write to a file.
+    
+    :param data: the file data (in bytes) to be encrypted
+    :param password: the end-user defined password used to derive the master key
+    :param header: a dictionary containing the required information for encryption
+    :return: a serialized dictionary containing encrypted data and required decryption information
+    '''
+    # Generate the master key and two derived keys for encryption and hmac
+    master_key = PBKDF2(password, header["salt"][0:16], header["key_size"], count=header["count"], hmac_hash_module=header["hashmode"])
+    encrypt_key = PBKDF2(master_key, header["salt"][16:32], header["key_size"], count=1, hmac_hash_module=header["hashmode"])
+    mac_key = PBKDF2(master_key, header["salt"][32:], header["key_size"], count=1, hmac_hash_module=header["hashmode"])
     
     # Generate initialization vector
-    initial_v = get_random_bytes(args["iv_size"])
+    initial_v = get_random_bytes(header["iv_size"])
+    # Create cipher object in CBC mode with specified iv and derived encryption key
+    cipher = header["crypto_alg"].new(encrypt_key, header["crypto_alg"].MODE_CBC, iv=initial_v)
+    # Encrypt and create ciphertext
+    # Pad data to match block size for encryption mode
+    ciphertext = cipher.encrypt(pad(data, header["crypto_alg"].block_size))
     
-    # Create AES cipher object in CBC mode with iv and encryption key
-    cipher = args["crypto_alg"].new(e_key, args["crypto_alg"].MODE_CBC, iv=initial_v)
-    # Encrypt and create ciphertext - pad data to match block size for CBC
-    ciphertext = cipher.encrypt(pad(data, args["crypto_alg"].block_size))
-    
-    # Create HMAC
-    mac = HMAC.new(h_key, digestmod=args["hashmode"])
+    # Create HMAC object
+    mac = HMAC.new(mac_key, digestmod=header["hashmode"])
     # Update the hmac to cover the iv and ciphertext
     mac.update(initial_v + ciphertext)
-    args["data"] = mac.digest() + initial_v + ciphertext
+    header["data"] = mac.digest() + initial_v + ciphertext
     
     # Convert module obj to str for serialization
-    args["hashmode"] = args["hashmode"].__name__
-    args["crypto_alg"] = args["crypto_alg"].__name__
+    header["hashmode"] = header["hashmode"].__name__
+    header["crypto_alg"] = header["crypto_alg"].__name__
     
-    # serialize dictionary to byte stream
-    encrypted_structure = pickle.dumps(args) 
+    # Serialize dictionary to byte stream for file writing
+    encrypted_structure = pickle.dumps(header) 
     
     return encrypted_structure
 
-'''
-Decrypt File encrypted by this program
-'''
-def decrypt_file(data, password):    
-    args = pickle.loads(data)
-    # dynamically load appropriate modules
-    args["hashmode"] = importlib.import_module(args["hashmode"])
-    args["crypto_alg"] = importlib.import_module(args["crypto_alg"])
-    # Get master decrypt key
-    md_key = PBKDF2(password, args["salt"][0:16], args["key_size"], count=args["count"], hmac_hash_module=args["hashmode"])
+def decrypt_file(data, password):
+    '''
+    Decrypt a file that was encrypted by this same program. The output will be written back to a file
+    named by that specified in the end-user provided commandline arguments.
+    
+    :param data: the encrypted file data (in bytes) to be decrypted
+    :param password: the end-user defined password used to derive the master key
+    :return: decrypted file data in it's original form
+    '''
+    # De-serialize the file into a dictionary object    
+    header = pickle.loads(data)
+    # dynamically load appropriate encryption and hashing modules
+    header["hashmode"] = importlib.import_module(header["hashmode"])
+    header["crypto_alg"] = importlib.import_module(header["crypto_alg"])
+    # Derive the master key
+    master_key = PBKDF2(password, header["salt"][0:16], header["key_size"], count=header["count"], hmac_hash_module=header["hashmode"])
     # Derive encrypt key and hmac key
-    de_key = PBKDF2(md_key, args["salt"][16:32], args["key_size"], count=1, hmac_hash_module=args["hashmode"])
-    dh_key = PBKDF2(md_key, args["salt"][32:], args["key_size"], count=1, hmac_hash_module=args["hashmode"])
+    encrypt_key = PBKDF2(master_key, header["salt"][16:32], header["key_size"], count=1, hmac_hash_module=header["hashmode"])
+    mac_key = PBKDF2(master_key, header["salt"][32:], header["key_size"], count=1, hmac_hash_module=header["hashmode"])
 
     # Create a decrypt hmac object
-    d_mac = HMAC.new(dh_key, digestmod=args["hashmode"])
-    d_mac.update(args["data"][d_mac.digest_size:])
+    mac = HMAC.new(mac_key, digestmod=header["hashmode"])
+    mac.update(header["data"][mac.digest_size:])
 
-    # Retrieve original hmac
-    o_mac = args["data"][0:d_mac.digest_size]
+    # Retrieve the original hmac
+    o_mac = header["data"][0:mac.digest_size]
 
     # Compare and authenticate HMAC
     try:
-        d_mac.verify(o_mac)
+        mac.verify(o_mac)
         print("Message is authentic")
     except ValueError:
         print("Message not authenticated")
         sys.exit(1)
-    # Retrieve iv
-    iv = args["data"][d_mac.digest_size:d_mac.digest_size + args["iv_size"]]
-    # Decryption cipher object
-    d_cipher = args["crypto_alg"].new(de_key, args["crypto_alg"].MODE_CBC, iv)
-    # Decrypted bytes - Remove block size padding
-    d_crypt_data = unpad(d_cipher.decrypt(args["data"][d_mac.digest_size + args["iv_size"]:]), args["crypto_alg"].block_size)    
+        
+    # Retrieve the iv - separating it from the encrypted data
+    iv = header["data"][mac.digest_size:mac.digest_size + header["iv_size"]]
+    # Create a cipher object for decryption
+    d_cipher = header["crypto_alg"].new(encrypt_key, header["crypto_alg"].MODE_CBC, iv)
+    # Decrypt the data and remove block size padding
+    d_crypt_data = unpad(d_cipher.decrypt(header["data"][mac.digest_size + header["iv_size"]:]), header["crypto_alg"].block_size)
+        
     return d_crypt_data
 
-'''
-Handles opening and writing to file and path based on args
-'''
-def file_handler(options, args):
-    # Open file to encrypt in bytes
-    file = open(options.filepath, "rb")
-    data = file.read()
-    file.close()
+def file_handler(args, header):
+    '''
+    Handles opening files, directing the data to the appropriate encryption or decryption functions.
+    This function also handles writing the returned data (encrypted or decrypted) to a file.
+    
+    :param args: namespace object created from the commandline arguments
+    :param header: dictionary object containing encryption specifications
+    '''
+    # Open file to encrypt or decrypt, in bytes
+    try:
+        with open(args.filepath, "rb") as file:
+            data = file.read()
+            file.close()
+    except IOError:
+        print("File could not be located.")
+        sys.exit(1)
     
     # Decide to encrypt or decrypt
-    if options.mode == 'e':
+    if args.mode == 'e':
         # Encrypt the file
-        ecn_file = encrypt_file(data, options.password, args)
+        ecn_file = encrypt_file(data, args.password, header)
         # Write and save encrypted data bytes as file
-        file = open(options.filepath + args["extension"], "wb")
+        file = open(args.filepath + header["extension"], "wb")
         file.write(ecn_file)
         file.close()
     else:
-        dec_file = decrypt_file(data, options.password)
-        file = open(options.output, "wb")
+        dec_file = decrypt_file(data, args.password)
+        file = open(args.output, "wb")
         file.write(dec_file)
         file.close()
         
-options = get_arguments()
-args = config_handler()
-file_handler(options, args)
+if __name__ == '__main__':
+    # Retrieve commandline arguments
+    args = get_arguments()
+    # Retrieve configuration arguments and build header
+    header = config_handler()
+    # Handle file and encryption or decryption
+    file_handler(args, header)
         
         
     
